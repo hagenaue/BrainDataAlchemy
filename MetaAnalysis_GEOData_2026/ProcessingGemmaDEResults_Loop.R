6379eb7e82203f91587441851e55c99bfd653a88
Megan Hastings Hagenauer <hagenaue@umich.edu>
2026-07-26 16:04:04 -0400

    Update ProcessingGemmaDEResults_Loop.R
    Updated to fix bugs and call packages

--- MetaAnalysis_GEOData_2026/ProcessingGemmaDEResults_Loop.R
+++ MetaAnalysis_GEOData_2026/ProcessingGemmaDEResults_Loop.R
@@ -1,4 +1,3 @@
-
 #The fast and dirty version of processing the Gemma DE files
 #Megan Hagenauer 
 #July 23 2026
@@ -16,21 +15,34 @@
 
 library(gemma.R)
 
-install.packages("tidyverse")
+if (!requireNamespace("tidyverse", quietly = TRUE)) {
+  install.packages("tidyverse")
+}
 
 library(tidyverse)
 
-install.packages("dplyr")
+if (!requireNamespace("dplyr", quietly = TRUE)) {
+  install.packages("dplyr")
+}
 
 library(dplyr)
 
 ########################
 
+#Next, download the functions from our repository and put them in your working directory:
 
-#Next, download the functions from the 2024 BDA github repository and put them in your working directory
-https://github.com/hagenaue/BrainDataAlchemy/tree/main/MetaAnalysis_GemmaDEResults_2024
+#Function FilteringDEResults_GoodAnnotation:
+#https://github.com/hagenaue/BrainDataAlchemy/blob/main/MetaAnalysis_GemmaDEResults_2024/Function_FilteringDEResults_GoodAnnotation.R
 
-#source the functions from their files:
+#Function ExtractingDEResultsForContrasts (this one was updated/debugged for 2026):
+#https://github.com/hagenaue/BrainDataAlchemy/blob/main/MetaAnalysis_GEOData_2026/Function_ExtractingDEResultsForContrasts.R
+
+#Function_CollapsingDEResults_OneResultPerGene:
+#https://github.com/hagenaue/BrainDataAlchemy/blob/main/MetaAnalysis_GemmaDEResults_2024/Function_CollapsingDEResults_OneResultPerGene.R
+
+##########################
+
+#Then source the functions from their files in your working directory:
 
 source("Function_FilteringDEResults_GoodAnnotation.R")
 
@@ -49,9 +61,11 @@ for(i in c(1:length(differentials)) ){
   print(i)
   
   DE_Results<-differentials[[i]]
+  CurrentResultSet<-names(differentials)[i]
+  
   FilteringDEResults_GoodAnnotation(DE_Results)
 
-  ExtractingDEResultsForContrasts(DE_Results_GoodAnnotation, Contrasts_Log2FC, Contrasts_Tstat, ResultSet_contrasts)
+  ExtractingDEResultsForContrasts(DE_Results_GoodAnnotation, Contrasts_Log2FC, Contrasts_Tstat, ResultSet_contrasts, CurrentResultSet)
   
   CollapsingDEResults_OneResultPerGene(GSE_ID, DE_Results_GoodAnnotation, ComparisonsOfInterest, NamesOfFoldChangeColumns, NamesOfTstatColumns)
   
